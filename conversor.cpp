f9e2a519dcd6a6e1bed7b4e970e633105bbbd31b
leofantucci <leo.fantucci@gmail.com>
2025-05-09 14:19:37 -0300

    commit 0

--- conversor.cpp
+++ conversor.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
+void temperaturas(){
+    cout << "Celcius (C)" << endl;
+    cout << "Fahrenheit (F)"<< endl;
+    cout << "Kelvin (K)" << endl;
+}
+
 int main(){
-    return 0;.
+    float temp;
+    char To, Tc;
+    cout << "Qual a escala da sua temperatura original?" << endl;
+    temperaturas();
+    cin >> To;
+    cout << "Insira a temperatura: ";
+    cin >> temp;
+    cout << "Escolha para qual temperatura você deseja converter" << endl;
+    temperaturas();
+    cin >> Tc;
+    return 0;
 }
